3fba0493d23440d84a23b27aad345488761598d4
BrendanSkyLiang <87059304+BrendanSkyLiang@users.noreply.github.com>
2021-07-21 15:11:15 +0800

    Update pullMacroMarketProperties.py

--- pullMacroMarketProperties.py
+++ pullMacroMarketProperties.py
@@ -40,23 +40,23 @@ equities_all_categories = fd.show_options('equities')
 
 
 COUN = 'United States'
-INDUS = 'Aerospace & Defense'
+INDUS = 'Biotechnology'
 
 
 companylist = fd.select_equities(country=COUN, industry=INDUS)
 
 '-----------------------------------------------------------------------------------------------------------------------------'
-# # can be deactivitaed if json file exists
-# fundamentals = {}
-# for symbol in companylist:
-#     fundamentals[symbol] = get_json("https://finance.yahoo.com/quote/" + symbol)
+# can be deactivitaed if json file exists
+fundamentals = {}
+for symbol in companylist:
+    fundamentals[symbol] = get_json("https://finance.yahoo.com/quote/" + symbol)
 
-# # stock_data = download(list(companylist))
+# stock_data = download(list(companylist))
 
-# fundamentals = {k:v for k,v in fundamentals.items() if not '.' in k}
+fundamentals = {k:v for k,v in fundamentals.items() if not '.' in k}
 
-# with open(COUN + INDUS + '.json', 'w') as fp:
-#     json.dump(fundamentals, fp)
+with open(COUN + INDUS + '.json', 'w') as fp:
+    json.dump(fundamentals, fp)
 
 '------------------------------------------------------------------------------------------------------------------------------'
 
@@ -65,54 +65,58 @@ with open(COUN + INDUS + '.json', 'r') as fp:
 
 '---------------------------------------------------------------------------------------------------------------------------------'
 
-housing = 'defaultKeyStatistics'
-item = 'priceToBook'
+defaultKey = ['beta', 'enterpriseToEbitda','enterpriseToRevenue','pegRatio','priceToBook','profitMargins','forwardPE']
 
-long_name = []
-parameter = []
+for i in range(len(defaultKey)):
+    item = defaultKey[i]
 
+    housing = 'defaultKeyStatistics'
 
-for symbol in fundamentals:
-    if len(fundamentals[symbol]) < 10:
-        continue
-    elif fundamentals[symbol][housing] is None:
-        continue
-    elif fundamentals[symbol][housing][item] is None:
-        continue
+    long_name = []
+    parameter = []
     
-    a = fundamentals[symbol][housing][item]
-    b = fundamentals[symbol]['quoteType']['longName']
-    long_name.append(b)
-    parameter.append(a)
+    for symbol in fundamentals:
+        if len(fundamentals[symbol]) < 10:
+            continue
+        elif fundamentals[symbol][housing] is None:
+            continue
+        elif fundamentals[symbol][housing][item] is None:
+            continue
+        
+        a = fundamentals[symbol][housing][item]
+        b = fundamentals[symbol]['quoteType']['longName']
+        long_name.append(b)
+        parameter.append(a)
+    
+        if fundamentals is None:
+            continue
+    
+    
+    # long_name = list(dict.fromkeys(long_name))
+    # parameter = list(dict.fromkeys(parameter))
+    
+    y_pos = np.arange(len(parameter))
+    
+    
+    # Graphing Controls
+    
+    plt.barh(y_pos, parameter, color = Cs)
+    plt.yticks(y_pos, long_name, rotation='horizontal')
+    plt.tick_params(axis = 'y', labelsize = 4)
+    plt.margins(0.2)
+    plt.xlabel(item)
+    plt.grid(b = True)
+    plt.subplots_adjust(bottom=0.15)
+    
+    
+    plt.tight_layout()
+    plt.savefig(COUN + ' ' +  INDUS + ' ' + item + '.png', dpi=600)
+    plt.show()
+    
+    # Export Data to csv
+    Combined = pd.DataFrame({'long_name': long_name, item : parameter})
+    Combined.to_csv(COUN + ' ' +  INDUS + ' ' + item + '.csv', index = False)
 
-    if fundamentals is None:
-        continue
-
-
-# long_name = list(dict.fromkeys(long_name))
-# parameter = list(dict.fromkeys(parameter))
-
-y_pos = np.arange(len(parameter))
-
-
-# Graphing Controls
-
-plt.barh(y_pos, parameter, color = Cs)
-plt.yticks(y_pos, long_name, rotation='horizontal')
-plt.tick_params(axis = 'y', labelsize = 4)
-plt.margins(0.2)
-plt.xlabel(item)
-plt.grid(b = True)
-plt.subplots_adjust(bottom=0.15)
-
-
-plt.tight_layout()
-plt.savefig(COUN + ' ' +  INDUS + ' ' + item + '.png', dpi=600)
-plt.show()
-
-# Export Data to csv
-Combined = pd.DataFrame({'long_name': long_name, item : parameter})
-Combined.to_csv(COUN + ' ' +  INDUS + ' ' + item + '.csv', index = False)
 
 
 
